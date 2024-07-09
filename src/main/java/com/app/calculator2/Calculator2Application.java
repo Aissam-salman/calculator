d775df8c30c1133578066199a4bfc9655d1de03f
Aissam-salman <nasawi.lmj@gmail.com>
2024-07-09 14:39:53 +0200

    Jenkins deploy EC2

--- src/main/java/com/app/calculator2/Calculator2Application.java
+++ src/main/java/com/app/calculator2/Calculator2Application.java
@@ -5,9 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class Calculator2Application {
-
     public static void main(String[] args) {
         SpringApplication.run(Calculator2Application.class, args);
     }
-
 }
