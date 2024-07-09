6a9f2d6cdadf613d0818e212a6f1273418f56d44
Aissam-salman <nasawi.lmj@gmail.com>
2024-07-09 13:57:08 +0200

    Jenkins deploy EC2

--- src/test/java/com/app/calculator2/Calculator2ApplicationTests.java
+++ src/test/java/com/app/calculator2/Calculator2ApplicationTests.java
@@ -24,7 +24,7 @@ class Calculator2ApplicationTests {
 
     @Test
     void testSubtract() {
-        assertEquals(2, calculatorService.subtract(4, 2));
+        assertEquals(3, calculatorService.subtract(5, 2));
     }
 
     @Test
